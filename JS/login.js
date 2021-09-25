e22ad93691d86abababed9fc0e5193fe4b9b678e
Sneha <snehashalythomas@gmail.com>
2021-09-25 11:06:27 +0530

    First commit:First version

---
+++ JS/login.js
@@ -0,0 +1,31 @@
+function validate(check){
+    check();
+}
+function check(){
+    var uname = document.getElementById("uname").value;
+    var psw = document.getElementById("psw").value;
+
+    if (uname == "admin" && psw == 12345){
+        window.location.href = 'main.html';
+        return false;
+}
+    else{
+    window.location.href = 'login.html';
+    return false;
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
---
+++ JS/main.js
@@ -0,0 +1,50 @@
+
+function tableFromJson(){
+  $(document).ready(function(){
+
+  $.getJSON( "https://jsonplaceholder.typicode.com/todos", function( data ) {
+    var output ="";
+    for(var i=0;i<data.length;i++){
+      if(data[i].completed==true){
+        output+='<tr><ul style="list-style-type: none;"><li><input type="checkbox"  id="check" name="test" checked disabled>' +  `${data[i].title}    ` + '</li></ul></tr>';
+
+      }
+      else{
+        output+='<ul style="list-style-type: none;"><li><input type="checkbox" onchange="handleChange(this)" id="checks" name="test">' +  `${data[i].title}`  + '</li></ul>';
+      }
+    }
+    $(".container").html(output);
+  });
+  
+  $("input[type=checkbox][name=test]").change(function() {
+    alert("jooo");
+  var numberOfChecked = $('input:checkbox:checked').length;
+  var totalCheckboxes = $('input:checkbox').length;
+  console.log(totalCheckboxes);
+  });
+});
+
+}
+
+function logout(){ 
+      window.location.href = 'login.html';
+     
+}
+
+function handleChange(checkbox) {  
+ var Prom=new Promise(function(resolve,reject){
+  var q=document.querySelectorAll("input:checked").length;
+  if(q==95){
+    resolve("Congrats. 5 Tasks have been Successfully Completed ");
+  }
+  
+})
+Prom
+.then(function(s){
+  alert(s);
+})
+.catch();
+}
+
+
+
